0ba62d7c86498f7695f821ed33363ad102a41004
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2020-09-01 22:42:10 +0200

    Added requirements.txt for binder.

--- setup.py
+++ setup.py
@@ -2,12 +2,16 @@ import setuptools
 
 setuptools.setup(
     name="bunny_lab",
-    version="0.0.1",
+    version="0.0.3",
     author="Simon Pfreundschuh",
     author_email="simon.pfreundschuh@chalmers.se",
     description="An interactive Python quest to save the bunnies.",
     url="https://github.com/simonpf/bunny_lab",
     packages=setuptools.find_packages(),
+    package_data={
+        "": ["*.txt", "*.rst"],
+    },
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
